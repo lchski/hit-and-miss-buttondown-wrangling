f7d31e7bd9d3a6b00dbb42cc83acc301ba9fd532
Lucas Cherkewski <lucas@ecustom.ca>
2018-12-19 12:04:54 -0500

    merge in email metadata

--- process.R
+++ process.R
@@ -2,10 +2,17 @@ library(tidyverse)
 library(readtext)
 
 email_index <- read_csv("data/emails.csv")
+email_index <- email_index %>%
+  arrange(publish_date) %>%
+  mutate(email_number = row_number())
 
 newsletters <- readtext("data/newsletters/*")
 newsletters <- tibble(subject = newsletters$doc_id, text = newsletters$text) %>%
   mutate(subject = gsub("^Hit and Miss #", "", subject)) %>%
   separate(subject, into = c("id", "subject"), sep = ": ") %>%
   mutate(id = parse_number(id)) %>%
-  arrange(id)
+  arrange(id) %>%
+  inner_join(
+    email_index %>% select(email_number, publish_date),
+    by = c("id" = "email_number")
+  )
